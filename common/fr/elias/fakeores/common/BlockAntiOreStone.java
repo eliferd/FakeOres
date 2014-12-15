a9a03e51a4060e5173b46909de87731f47f8cf7a
elias54 <elias5401@hotmail.fr>
2014-12-15 23:08:23 +0100

    Fixed Anti Ores Stone

--- common/fr/elias/fakeores/common/BlockAntiOreStone.java
+++ common/fr/elias/fakeores/common/BlockAntiOreStone.java
@@ -13,115 +13,130 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockAntiOreStone extends Block {
+public class BlockAntiOreStone extends Block
+{
 
-	public BlockAntiOreStone()
-	{
-		super(Material.rock);
-		setCreativeTab(FakeOres.fakeOresTab);
-		this.setBlockBounds(0.0015F, 0.0F, 0.0015F, 0.9985F, 1F, 0.9985F);
-		this.setTickRandomly(true);
-		
-	}
-    public void onEntityCollidedWithBlock(World par1World, BlockPos pos, Entity par5Entity) 
+    public BlockAntiOreStone()
     {
-    	actionHandler(par5Entity);
+        super(Material.rock);
+        setCreativeTab(FakeOres.fakeOresTab);
     }
     
+    public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state)
+    {
+        float f = 0.025F;
+        return new AxisAlignedBB((double)((float)pos.getX() + f), (double)pos.getY(), (double)((float)pos.getZ() + f), (double)((float)(pos.getX() + 1) - f), (double)((float)(pos.getY() + 1) - f), (double)((float)(pos.getZ() + 1) - f));
+    }
+
+    public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity)
+    {
+        System.out.println("ON_ENTITY_COLLIDED_WITh_BLOCK");
+        actionHandler(entity);
+        System.out.println("ON_ENTITY_COLLIDED_WITh_BLOCK_AFTER");
+    }
+
     public void onBlockAdded(World world, BlockPos pos, IBlockState state)
     {
-    	int x = pos.getX(),
-    		y = pos.getY(),
-    		z = pos.getZ();
-    	removeNearBlock(world, x + 1, y, z);
-    	removeNearBlock(world, x, y + 1, z);
-    	removeNearBlock(world, x, y, z + 1);
-    	removeNearBlock(world, x - 1, y, z);
-    	removeNearBlock(world, x, y - 1, z);
-    	removeNearBlock(world, x, y, z - 1);
-    }
-    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
-    {
-    	int x = pos.getX(),
-    		y = pos.getY(),
-    		z = pos.getZ();
-    	removeNearBlock(worldIn, x + 1, y, z);
-    	removeNearBlock(worldIn, x, y + 1, z);
-    	removeNearBlock(worldIn, x, y, z + 1);
-    	removeNearBlock(worldIn, x - 1, y, z);
-    	removeNearBlock(worldIn, x, y - 1, z);
-    	removeNearBlock(worldIn, x, y, z - 1);
-    	AxisAlignedBB aabb = new AxisAlignedBB((double)x, (double)y, (double)z, (double)(x + 1), (double)(y + 1), (double)(z + 1)).expand(1, 1, 1);
-    	List list = worldIn.getEntitiesWithinAABB(EntityOres.class, aabb);
-    	if(list.size() > 0)
-    	{
-    		Entity entity = (Entity)list.get(0);
-        	if(entity instanceof EntityOres)
-        	{
-        		entity.attackEntityFrom(FakeOres.antiorestone, Float.MAX_VALUE);
-        	}
-    	}
-    	super.updateTick(worldIn, pos, state, rand);
+        System.out.println("ON_BLOCK_ADDED");
+        removeNearBlock(world, pos.add(1, 0, 0));
+        removeNearBlock(world, pos.add(0, 1, 0));
+        removeNearBlock(world, pos.add(0, 0, 1));
+        removeNearBlock(world, pos.add(-1, 0, 0));
+        removeNearBlock(world, pos.add(0, -1, 0));
+        removeNearBlock(world, pos.add(0, 0, -1));
+        System.out.println("ON_BLOCK_ADDED_AFTER");
     }
     
-    public void onBlockDestroyedByPlayer(World par1World, BlockPos pos, IBlockState state) 
+    public void onNeighborBlockChange(World worldIn, BlockPos pos, IBlockState state, Block neighborBlock) 
     {
-    	if(par1World.isRemote)
-    	{
-    		FMLClientHandler.instance().getClient().theWorld.spawnParticle(EnumParticleTypes.LAVA, pos.getX(), pos.getY(), pos.getZ(), 0.0D, 0.02D, 0.0D);
-    	}
+        removeNearBlock(worldIn, new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ()));
+        removeNearBlock(worldIn, new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ()));
+        removeNearBlock(worldIn, new BlockPos(pos.getX(), pos.getY(), pos.getZ() + 1));
+        removeNearBlock(worldIn, new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ()));
+        removeNearBlock(worldIn, new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ()));
+        removeNearBlock(worldIn, new BlockPos(pos.getX(), pos.getY(), pos.getZ() - 1));
     }
-    public void removeNearBlock(World world, int x, int y, int z)
+
+
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
     {
-    	if(world.getBlockState(new BlockPos(x, y, z)) == FakeOres.fakeDiamond)
-    	{
-    		world.setBlockToAir(new BlockPos(x, y, z));
-    	}else
-    	if(world.getBlockState(new BlockPos(x, y, z)) == FakeOres.fakeEmerald)
-    	{
-    		world.setBlockToAir(new BlockPos(x, y, z));
-    	}else
-    	if(world.getBlockState(new BlockPos(x, y, z)) == FakeOres.fakeGold)
-    	{
-    		world.setBlockToAir(new BlockPos(x, y, z));
-    	}else
-    	if(world.getBlockState(new BlockPos(x, y, z)) == FakeOres.fakeIron)
-    	{
-    		world.setBlockToAir(new BlockPos(x, y, z));
-    	}else
-    	if(world.getBlockState(new BlockPos(x, y, z)) == FakeOres.fakeCoal)
-    	{
-    		world.setBlockToAir(new BlockPos(x, y, z));
-    	}else
-    	if(world.getBlockState(new BlockPos(x, y, z)) == FakeOres.fakeRedstone)
-    	{
-    		world.setBlockToAir(new BlockPos(x, y, z));
-    	}else
-    	if(world.getBlockState(new BlockPos(x, y, z)) == FakeOres.fakeQuartz)
-    	{
-    		world.setBlockToAir(new BlockPos(x, y, z));
-    	}else
-    	if(world.getBlockState(new BlockPos(x, y, z)) == FakeOres.fakeLapis)
-    	{
-    		world.setBlockToAir(new BlockPos(x, y, z));
-    	}else{
-    		System.out.print("error");
-    	}
+        System.out.println("UPDATE_TICK");
+        AxisAlignedBB aabb = new AxisAlignedBB(pos, pos.add(1, 1, 1)).expand(1, 1, 1);
+
+        List list = worldIn.getEntitiesWithinAABB(EntityOres.class, aabb);
+        if(list.size() > 0)
+        {
+            Entity entity = (Entity)list.get(0);
+            if(entity instanceof EntityOres)
+            {
+                entity.attackEntityFrom(FakeOres.antiorestone, Float.MAX_VALUE);
+            }
+        }
+        super.updateTick(worldIn, pos, state, rand);
+        System.out.println("UPDATE_TICK_AFTER");
     }
+
+    public void onBlockDestroyedByPlayer(World world, BlockPos pos, IBlockState state)
+    {
+        if(world.isRemote)
+        {
+            world.spawnParticle(EnumParticleTypes.LAVA, pos.getX(), pos.getY(), pos.getZ(), 0.0D, 0.02D, 0.0D);
+        }
+    }
+
+    public void removeNearBlock(World world, BlockPos pos)
+    {
+        if(world.getBlockState(pos).getBlock() == FakeOres.fakeDiamond)
+        {
+            world.setBlockToAir(pos);
+        }
+        if(world.getBlockState(pos).getBlock() == FakeOres.fakeEmerald)
+        {
+            world.setBlockToAir(pos);
+        }
+        if(world.getBlockState(pos).getBlock() == FakeOres.fakeGold)
+        {
+            world.setBlockToAir(pos);
+        }
+        if(world.getBlockState(pos).getBlock() == FakeOres.fakeIron)
+        {
+            world.setBlockToAir(pos);
+        }
+        if(world.getBlockState(pos).getBlock() == FakeOres.fakeCoal)
+        {
+            world.setBlockToAir(pos);
+        }
+        if(world.getBlockState(pos).getBlock() == FakeOres.fakeRedstone)
+        {
+            world.setBlockToAir(pos);
+        }
+        if(world.getBlockState(pos).getBlock() == FakeOres.fakeQuartz)
+        {
+            world.setBlockToAir(pos);
+        }
+        if(world.getBlockState(pos).getBlock() == FakeOres.fakeLapis)
+        {
+            world.setBlockToAir(pos);
+        }
+    }
+
     public void actionHandler(Entity entity)
     {
-    	if(!(entity instanceof EntityItem))
-    	{
-    		entity.attackEntityFrom(FakeOres.antiorestone, 18F);
-    	}
-    	if(entity instanceof EntityOres)
-    	{
-    		entity.attackEntityFrom(FakeOres.antiorestone, Float.MAX_VALUE);
-    	}
+        if(!(entity instanceof EntityItem))
+        {
+            entity.attackEntityFrom(FakeOres.antiorestone, 18F);
+        }
+        if(entity instanceof EntityOres)
+        {
+            entity.attackEntityFrom(FakeOres.antiorestone, Float.MAX_VALUE);
+        }
     }
+
     public boolean isOpaqueCube()
     {
-    	return false;
+        return false;
     }
 }
